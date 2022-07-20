fbf58f0f4d438c6274069fcb3a4f1d41d0f67408
David Horsley <david.horsley@hydro.com.au>
2022-07-21 09:38:24 +1000

    remove InfoZip, add support for extraced zips

--- src/PLEXOSUtils.jl
+++ src/PLEXOSUtils.jl
@@ -1,9 +1,8 @@
 module PLEXOSUtils
 
 import EzXML: Document, eachelement, namespace, Node, nodecontent, parsexml
-import InfoZIP: Archive, open_zip
 
-export open_plexoszip, PLEXOSSolutionDataset, PLEXOSSolutionDatasetSummary
+export open_plexos, open_plexoszip, PLEXOSSolutionDataset, PLEXOSSolutionDatasetSummary
 
 struct PLEXOSTable
 
@@ -16,57 +15,37 @@ struct PLEXOSTable
     indexoffset::Int
 
     PLEXOSTable(name::String, fieldname::Symbol, fieldtype::Symbol,
-                loadorder::Int,
-                identifier::Union{String,Nothing}=nothing,
-                timestampfield::Union{Symbol,Nothing}=nothing,
-                indexoffset::Int=0
+        loadorder::Int,
+        identifier::Union{String,Nothing}=nothing,
+        timestampfield::Union{Symbol,Nothing}=nothing,
+        indexoffset::Int=0
     ) = new(name, fieldname, fieldtype, loadorder,
-            identifier, timestampfield, indexoffset)
+        identifier, timestampfield, indexoffset)
 
 end
 
-plexostables = [
-
-    PLEXOSTable("t_config", :configs, :PLEXOSConfig, 1),
+plexostables = [PLEXOSTable("t_config", :configs, :PLEXOSConfig, 1),
     PLEXOSTable("t_unit", :units, :PLEXOSUnit, 1, "unit_id", nothing, 1),
     PLEXOSTable("t_timeslice", :timeslices, :PLEXOSTimeslice, 1, "timeslice_id", nothing, 1),
-    PLEXOSTable("t_model", :models, :PLEXOSModel, 1, "model_id"),
-
-    PLEXOSTable("t_sample", :samples, :PLEXOSSample, 1, "sample_id", nothing, 4),
-    PLEXOSTable("t_sample_weight", :sampleweights, :PLEXOSSampleWeight, 2),
-
-    PLEXOSTable("t_class_group", :classgroups, :PLEXOSClassGroup, 1, "class_group_id"),
+    PLEXOSTable("t_model", :models, :PLEXOSModel, 1, "model_id"), PLEXOSTable("t_sample", :samples, :PLEXOSSample, 1, "sample_id", nothing, 4),
+    PLEXOSTable("t_sample_weight", :sampleweights, :PLEXOSSampleWeight, 2), PLEXOSTable("t_class_group", :classgroups, :PLEXOSClassGroup, 1, "class_group_id"),
     PLEXOSTable("t_class", :classes, :PLEXOSClass, 2, "class_id"),
-    PLEXOSTable("t_category", :categories, :PLEXOSCategory, 3, "category_id"),
-
-    PLEXOSTable("t_attribute", :attributes, :PLEXOSAttribute, 3, "attribute_id"),
+    PLEXOSTable("t_category", :categories, :PLEXOSCategory, 3, "category_id"), PLEXOSTable("t_attribute", :attributes, :PLEXOSAttribute, 3, "attribute_id"),
     PLEXOSTable("t_collection", :collections, :PLEXOSCollection, 3, "collection_id"),
-    PLEXOSTable("t_property", :properties, :PLEXOSProperty, 4, "property_id"),
-
-    PLEXOSTable("t_object", :objects, :PLEXOSObject, 4, "object_id"),
+    PLEXOSTable("t_property", :properties, :PLEXOSProperty, 4, "property_id"), PLEXOSTable("t_object", :objects, :PLEXOSObject, 4, "object_id"),
     PLEXOSTable("t_membership", :memberships, :PLEXOSMembership, 5, "membership_id"),
-    PLEXOSTable("t_attribute_data", :attribute_data, :PLEXOSAttributeData, 5),
-
-    PLEXOSTable("t_custom_column", :customcolumns, :PLEXOSCustomColumn, 3, "column_id"),
-    PLEXOSTable("t_memo_object", :memos, :PLEXOSMemoObject, 5),
-
-    PLEXOSTable("t_period_0", :intervals, :PLEXOSPeriod0, 1, "interval_id", :datetime),
+    PLEXOSTable("t_attribute_data", :attribute_data, :PLEXOSAttributeData, 5), PLEXOSTable("t_custom_column", :customcolumns, :PLEXOSCustomColumn, 3, "column_id"),
+    PLEXOSTable("t_memo_object", :memos, :PLEXOSMemoObject, 5), PLEXOSTable("t_period_0", :intervals, :PLEXOSPeriod0, 1, "interval_id", :datetime),
     PLEXOSTable("t_period_1", :days, :PLEXOSPeriod1, 1, "day_id", :date),
     PLEXOSTable("t_period_2", :weeks, :PLEXOSPeriod2, 1, "week_id", :week_ending),
     PLEXOSTable("t_period_3", :months, :PLEXOSPeriod3, 1, "month_id", :month_beginning),
     PLEXOSTable("t_period_4", :years, :PLEXOSPeriod4, 1, "fiscal_year_id", :year_ending),
     PLEXOSTable("t_period_6", :hours, :PLEXOSPeriod6, 1, "hour_id", :datetime),
-    PLEXOSTable("t_period_7", :quarters, :PLEXOSPeriod7, 1, "quarter_id", :quarter_beginning),
-
-    PLEXOSTable("t_phase_1", :lt, :PLEXOSPhase1, 2),
+    PLEXOSTable("t_period_7", :quarters, :PLEXOSPeriod7, 1, "quarter_id", :quarter_beginning), PLEXOSTable("t_phase_1", :lt, :PLEXOSPhase1, 2),
     PLEXOSTable("t_phase_2", :pasa, :PLEXOSPhase2, 2),
     PLEXOSTable("t_phase_3", :mt, :PLEXOSPhase3, 2),
-    PLEXOSTable("t_phase_4", :st, :PLEXOSPhase4, 2),
-
-    PLEXOSTable("t_key", :keys, :PLEXOSKey, 6, "key_id"),
-    PLEXOSTable("t_key_index", :keyindices, :PLEXOSKeyIndex, 7)
-
-]
+    PLEXOSTable("t_phase_4", :st, :PLEXOSPhase4, 2), PLEXOSTable("t_key", :keys, :PLEXOSKey, 6, "key_id"),
+    PLEXOSTable("t_key_index", :keyindices, :PLEXOSKeyIndex, 7)]
 
 plexostables_lookup = Dict(x.name => x for x in plexostables)
 
--- src/utils.jl
+++ src/utils.jl
@@ -1,36 +1,79 @@
+using ZipFile
+
+function open_plexos(path::String, xmlname::String=defaultxml(zippath))
+    if !isnothing(match(r"\.zip$", path))
+        return open_plexoszip(path, xmlname)
+    end
+
+    println("opening as path")
+
+    println("parsing xml")
+    xmlfile = open(joinpath(path, xmlname), "r")
+    xml = parsexml(read(xmlfile, String))
+    close(xmlfile)
+
+    println("loading parsed xml")
+    data = PLEXOSSolutionDataset(xml)
+
+    println("reading bin files")
+    results = Dict{Int,Vector{UInt8}}()
+    for f in readdir(path, join=true)
+        rgx = match(r"t_data_(\d)\.BIN", f)
+        if isnothing(rgx)
+            continue
+        end
+        f = open(f, "r")
+        results[parse(Int, rgx[1])] = read(f)
+        close(f)
+    end
+    println("done")
+    return data, results
+end
+
 function open_plexoszip(zippath::String, xmlname::String=defaultxml(zippath))
 
-    resultsarchive = _open_plexoszip(zippath)
-    xml = parsexml(resultsarchive[xmlname])
-
+    r = _open_plexoszip(zippath)
+    xml = _read_xml_from_zip(r, xmlname)
     data = PLEXOSSolutionDataset(xml)
-    resultvalues = perioddata(resultsarchive)
+    resultvalues = perioddata(r)
+    close(r)
 
     return data, resultvalues
 
 end
 
+function _read_xml_from_zip(r::ZipFile.Reader, xmlname::String)
+    for f in r.files
+        if f.name == xmlname
+            return parsexml(read(f, String))
+        end
+    end
+    error("File $xmlname not in zip")
+end
+
 function _open_plexoszip(zippath::String)
     isfile(zippath) || error("$zippath does not exist")
-    archive = open_zip(zippath)
+    # archive = open_zip(zippath)
+    archive = ZipFile.Reader(zippath)
     return archive
 end
 
-defaultxml(zippath::String) = replace(basename(zippath), r".zip$"=>".xml")
+defaultxml(zippath::String) = replace(basename(zippath), r".zip$" => ".xml")
 
-function perioddata(archive::Archive)
+function perioddata(archive)
     results = Dict{Int,Vector{UInt8}}()
-    for filename in keys(archive)
-        rgx = match(r"t_data_(\d).BIN", filename)
+    for f in archive.files
+        rgx = match(r"t_data_(\d).BIN", f.name)
         isnothing(rgx) && continue
-        data = archive[filename]
+        data = read(f, String)
+        # data = f
         results[parse(Int, rgx[1])] = data
     end
     return results
 end
 
 function getchildtext(name::String, e::Node)
-    resultnode = findfirst("x:" * name, e, ["x"=>namespace(e)])
+    resultnode = findfirst("x:" * name, e, ["x" => namespace(e)])
     isnothing(resultnode) && error("$e does not have child $name")
     return nodecontent(resultnode)
 end
